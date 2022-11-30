1458141e9c668dbb1acac40721b239352ae0e90e
daliovic <dalidaliovic@gmail.com>
2022-11-30 03:35:05 +0300

    Fixed Calendly bug

--- src/~Home/components/UI/Contact.tsx
+++ src/~Home/components/UI/Contact.tsx
@@ -100,7 +100,7 @@ export default function Contact() {
              * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
              * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
              */
-            rootElement={document.getElementById('root')}
+            rootElement={document.getElementById('root') as HTMLElement}
             text='Click here to schedule!'
           />
         </div>
